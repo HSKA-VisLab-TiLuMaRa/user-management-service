12ebddd2e1d1720ebed1297860bbd0ccb7635b26
Raphael Hippe <info@raphaelhippe.de>
2018-05-09 11:57:14 +0200

    core functionality

--- README.md
+++ README.md
@@ -4,6 +4,11 @@
 
 This project is part of the microservice architecture which can be found [here](https://github.com/HSKA-VisLab-TiLuMaRa).
 
+## Maven commands
+- `mvn install`
+- `mvn spring-boot:run`
+- `mvn clean`
+
 ## user-management-service (Composite-Service)
 
 - offer a REST interface
--- src/main/java/de/hska/iwi/vslab/usermanagement/usermanagementservice/UserManagementServiceApplication.java
+++ src/main/java/de/hska/iwi/vslab/usermanagement/usermanagementservice/UserManagementServiceApplication.java
@@ -2,8 +2,16 @@ package de.hska.iwi.vslab.usermanagement.usermanagementservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableCircuitBreaker
+@EnableHystrixDashboard
+@RibbonClient("user-management")
 public class UserManagementServiceApplication {
 
 	public static void main(String[] args) {
